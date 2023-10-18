798b7895f002b432b5aa885f116e755b0e95ea11
sebeb-siz <sebebsiz93@gmail.com>
2023-10-18 18:39:34 +0300

    Second commit

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,15 +1,15 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    private Duck5 duck;
     @Autowired
-    public void setDuck(Duck5 duck) {
-        this.duck = duck;
-    }
+    @Qualifier("Duck5")
+    private Duck5 duck;
+
 
     @Override
     public String toString() {
